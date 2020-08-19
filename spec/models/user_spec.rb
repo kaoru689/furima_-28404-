da242135684eef92a6016947c25ffa736be368e0
kaoru689 <yokoyama.ai@gmail.com>
2020-08-19 16:28:45 +0900

    テストコードの修正変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,6 +57,11 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
+     it 'メールアドレスは@を含む必要があること' do
+        @user.email = "testtest.test"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+    end
     it "重複したemailが存在する場合登録できないこと" do
       @user.save
       another_user = FactoryBot.build(:user, email: @user.email)
