6343f55d24421749673cd806e438a028f38bdd04
kaoru689 <yokoyama.ai@gmail.com>
2020-08-20 14:57:49 +0900

    発送までの日数プルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,12 +7,13 @@ validates :category_id, presence: true
 validates :condition_id, presence: true
 validates :postage_id, presence: true
 validates :prefecture_id, presence: true
+validates :delivery_id, presence: true
 
 extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :postage
-  belongs_to_active_hash :prefecture
-  
+belongs_to_active_hash :category
+belongs_to_active_hash :condition
+belongs_to_active_hash :postage
+belongs_to_active_hash :prefecture
+belongs_to_active_hash :delivery
 
 end
