5a0da7a74ac1c603558b2f4f6c99b618ee0af289
kaoru689 <yokoyama.ai@gmail.com>
2020-08-20 14:24:10 +0900

    配送料の負担プルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,12 @@ has_one :buy
 
 validates :category_id, presence: true
 validates :condition_id, presence: true
+validates :postage_id, presence: true
 
 extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
   
 
 end
