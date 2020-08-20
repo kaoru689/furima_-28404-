edd48e9d35e8b9992b465171994f89ef507abb3b
kaoru689 <yokoyama.ai@gmail.com>
2020-08-20 14:40:54 +0900

    発送元の地域プルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,13 @@ has_one :buy
 validates :category_id, presence: true
 validates :condition_id, presence: true
 validates :postage_id, presence: true
+validates :prefecture_id, presence: true
 
 extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
   
 
 end
