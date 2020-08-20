90a9f4a7b6abc18206e81fbfb31efed86a520c01
kaoru689 <yokoyama.ai@gmail.com>
2020-08-20 11:42:26 +0900

    カテゴリープルダウン作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   # skip_before_action :authenticate_user! ,only: :index
   def index
     @items = Item.all
+    @items = Items.order("created_at DESC")
   end
   
   def new
@@ -9,6 +10,18 @@ class ItemsController < ApplicationController
   end
   
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:item_id)
+  end
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
 belongs_to :user
 has_one :buy
 
+extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  validates :title, :text, :category, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+
 end
