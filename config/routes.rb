95b8967e645164a87c28a84f78280e2b2504fce4
kaoru689 <yokoyama.ai@gmail.com>
2020-08-24 16:38:34 +0900

    絶対パスに変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
 
-
   resources :users , only: [:new ]
   resources :items , only:[:new ,:create, :show, :update ]
+
+  # devise_scope :user do
+  #   root to: 'destroy_user_session'
+  # end
+
+  # devise_scope :user do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
   
   end
