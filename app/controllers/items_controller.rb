ce4130f2966fb986e73691211a5d197b982aac43
kaoru689 <yokoyama.ai@gmail.com>
2020-08-26 12:28:43 +0900

    コントローラー設定追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,14 @@ class ItemsController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+      item.destroy
+      redirect_to root_path
+    end
+  end
    
 
   private
