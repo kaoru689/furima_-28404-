6853ae31f05356582c0e3389973bbf4e9cd829d2
kaoru689 <yokoyama.ai@gmail.com>
2020-08-30 22:46:36 +0900

    カード情報関係、コントローラー編集

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,28 +1,41 @@
 class BuysController < ApplicationController
 
-  def create
-    @buys = Buys.new(price: order_params[:price])
-    if @order.valid?
-      pay_item
-      @order.save
-      return redirect_to root_path
-    else
-      render :edit
-    end
+  def index
+   @item = Item.find(params[:item_id])
+   if current_user.id == @item.user.id
+      @item_id == nil
+      redirect_to root_path
+   end
   end
+
+  def new
+    @buys = BuysAddress.new
+  end
+
+def create
+  @buys = BuysAddress.new(buys_params)
+  @item = Item.find(params[:item_id])
+ if @buys.valid?
+    pay_item
+    @buys.save
+    redirect_to root_path
+ else
+  redirect_to item_path(@item.id)
+ end
+end
 
   private
 
   def buys_params
-    params.permit(:price, :token)
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
   
   def pay_item
-    Payjp.api_key = "PAYJP_SECRET_KEY"  # PAY.JPテスト秘密鍵
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency:'jpy'                 # 通貨の種類(日本円)
+      amount: @item.price,
+      card: params[:token],
+      currency:'jpy'
     )
   end
 end
--- app/models/buy.rb
+++ app/models/buy.rb
@@ -3,5 +3,4 @@ belongs_to :item
 belongs_to :user
 has_one :address
 
-
 end
