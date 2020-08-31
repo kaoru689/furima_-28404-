5745aa2cc5cebb8389a0673b6fb62e6cb41fe558
kaoru689 <yokoyama.ai@gmail.com>
2020-08-31 16:17:59 +0900

    Update buys_controller.rb

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,44 +1,45 @@
-class BuysController < ApplicationController
-  before_action :set_item , only: [:index, :create]
+  class BuysController < ApplicationController
+        before_action :set_item , only: [:index, :create]
 
-  def index
-   if current_user.id == @item.user.id
-      @item_id == nil
-      redirect_to root_path
-   end
-  end
+    def index
+    if  current_user.id == @item.user.id
+        @item_id == nil
+        redirect_to root_path
+    end
+    end
 
-  def new
-      @buys = BuysAddress.new
-  end
+    def new
+        @buys = BuysAddress.new
+    end
 
-  def create
-      @buys = BuysAddress.new(buys_params)
-   if @buys.valid?
-      pay_item
-      @buys.save
-      redirect_to root_path
-   else
-      redirect_to item_path(@item.id)
-   end
-  end
+    def create
+        @buys = BuysAddress.new(buys_params)
+    if  @buys.valid?
+        pay_item
+        @buys.save
+        redirect_to root_path
+    else
+        redirect_to item_path(@item.id)
+    end
+    end
 
-  private
+    private
 
-  def set_item
-      @item = Item.find(params[:item_id])
-  end
+    def set_item
+        @item = Item.find(params[:item_id])
+    end
 
-  def buys_params
-      params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    def buys_params
+        params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    end
+
+    
+    def pay_item
+        Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+        Payjp::Charge.create(
+        amount: @item.price,
+        card: params[:token],
+        currency:'jpy'
+      )
+    end
   end
-  
-  def pay_item
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-      amount: @item.price,
-      card: params[:token],
-      currency:'jpy'
-     )
-  end
-end
