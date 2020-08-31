b9d5163a80f6879a1d272c1a4bcffaa7ccd4475b
kaoru689 <yokoyama.ai@gmail.com>
2020-08-31 15:44:33 +0900

    訂正しました。

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,7 +1,7 @@
 class BuysController < ApplicationController
+  before_action :set_item , only: [:index, :create]
 
   def index
-   @item = Item.find(params[:item_id])
    if current_user.id == @item.user.id
       @item_id == nil
       redirect_to root_path
@@ -9,33 +9,36 @@ class BuysController < ApplicationController
   end
 
   def new
-    @buys = BuysAddress.new
+      @buys = BuysAddress.new
   end
 
-def create
-  @buys = BuysAddress.new(buys_params)
-  @item = Item.find(params[:item_id])
- if @buys.valid?
-    pay_item
-    @buys.save
-    redirect_to root_path
- else
-  redirect_to item_path(@item.id)
- end
-end
+  def create
+      @buys = BuysAddress.new(buys_params)
+   if @buys.valid?
+      pay_item
+      @buys.save
+      redirect_to root_path
+   else
+      redirect_to item_path(@item.id)
+   end
+  end
 
   private
 
+  def set_item
+      @item = Item.find(params[:item_id])
+  end
+
   def buys_params
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+      params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
   
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
       amount: @item.price,
       card: params[:token],
       currency:'jpy'
-    )
+     )
   end
 end
--- app/forms/buys_address.rb
+++ app/forms/buys_address.rb
@@ -3,10 +3,14 @@ class BuysAddress
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :user_id, :item_id
   
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :telephone_number, format: {with: /\A\d{11}\z/, message: "is telephone_number. exceeds 11 characters."}
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥 a-zA-Z0-9]+\z/, message: "is invalid. Input full-width characters."}
-    validates :house_number, format: { with: /[0-9\._¥+-]/, message: "is invalid. Input width characters."}
+    POSTALE_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
+    TELEPHONE_NUMBER_REGEX = /\A\d{11}\z/
+    CITY_REGEX = /\A[ぁ-んァ-ン一-龥 a-zA-Z0-9]+\z/
+    HOUSE_NUMBER_REGEX = /[0-9\._¥+-]/
+    validates :postal_code, format: {with: POSTALE_CODE_REGEX, message: "is invalid. Include hyphen(-)"}
+    validates :telephone_number, format: {with: TELEPHONE_NUMBER_REGEX, message: "is telephone_number. exceeds 11 characters."}
+    validates :city, format: { with: CITY_REGEX, message: "is invalid. Input full-width characters."}
+    validates :house_number, format: { with: HOUSE_NUMBER_REGEX, message: "is invalid. Input width characters."}
     end
    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     
