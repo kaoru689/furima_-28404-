50eb4e6cfa1d218241c68c84cf711582b4652e3b
kaoru689 <yokoyama.ai@gmail.com>
2020-09-04 14:28:02 +0900

    エラー文の日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ApplicationRecord
     KANA_REGEX = /\A[ァ-ヶー－]+\z/
     NICKNAME_REGEX = /\A[ぁ-んァ-ン一-龥 a-zA-Z0-9]+\z/
     PASSWORD_REGEX = /\A[a-z]+[\d]+[a-z\d]+\z/
-    validates :first_name, format: { with: NAME_REGEX, message: "is invalid. Input full-width characters."}
-    validates :last_name, format: { with: NAME_REGEX , message: "is invalid. Input full-width characters."}
-    validates :first_name_kana, format: { with: KANA_REGEX , message: "is invalid. Input full-width katakana characters."}
-    validates :last_name_kana, format: { with: KANA_REGEX , message: "is invalid. Input full-width katakana characters."}
-    validates :nickname, format: { with: NICKNAME_REGEX , message: "is invalid. Input width characters."}
-    validates :password, format: { with: PASSWORD_REGEX , message:"is invalid. Input half-width characters."}
+    validates :first_name, format: { with: NAME_REGEX, message: "は全角日本語で入力してください"}
+    validates :last_name, format: { with: NAME_REGEX , message: "は全角日本語で入力してください"}
+    validates :first_name_kana, format: { with: KANA_REGEX , message: "は全角カタカナで入力してください"}
+    validates :last_name_kana, format: { with: KANA_REGEX , message: "は全角カタカナで入力してください"}
+    validates :nickname, format: { with: NICKNAME_REGEX , message: "を入力してください"}
+    validates :password, format: { with: PASSWORD_REGEX , message:"は英数混合で入力してください"}
     validates :birthday
   end
 end
